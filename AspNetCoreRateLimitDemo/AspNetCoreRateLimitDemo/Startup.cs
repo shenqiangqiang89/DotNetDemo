091651a44279fa4a01ea5a38fc77337246a51a63
SHENQIANGQIANG <SHENQIANGQIANG@SH-SHENQIANGQIA>
2021-01-14 10:12:49 +0800

    微服务网关test

---
+++ AspNetCoreRateLimitDemo/AspNetCoreRateLimitDemo/Startup.cs
@@ -0,0 +1,82 @@
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreRateLimitDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+
+            services.AddMemoryCache();
+
+            services.Configure<IpRateLimitOptions>(options =>
+            {
+                options.HttpStatusCode = 429; //限流后返回的http状态码
+                options.QuotaExceededMessage = "请求过于频繁";//自定义限流后返回的http消息
+                options.GeneralRules = new List<RateLimitRule>
+                {
+                    //里配置的是IP限制，它允许有很多规则，这里我只用了一个：针对所有的资源，每5秒最多3次请求。
+                     new RateLimitRule
+                     {
+                         Endpoint="*",//所有
+                         Limit=3,//配额
+                         Period="5s"//间隔
+                     }
+                };
+            });
+
+            //注入计时器和规则
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNetCoreRateLimitDemo", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseIpRateLimiting();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNetCoreRateLimitDemo v1"));
+            }
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
